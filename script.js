8f259b94a28dc6c28ca7464b6445942604a165b0
Soumyaraj Dutta Banik <soumyarajduttabanik@gmail.com>
2026-09-23 02:44:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+const links=document.querySelectorAll('a[href^="#"]');links.forEach(a=>a.addEventListener('click',e=>{const el=document.querySelector(a.getAttribute('href'));if(el){e.preventDefault();el.scrollIntoView({behavior:'smooth'})}}));
